aef61fb3ea99c485cd506e802f1c8c6271bb27c9
Vy Nguyen <nguyen1446@sps81.org>
2020-01-30 10:04:41 -0800

    first commit

---
+++ SortSearchFun.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+void printArray(int[], int);
+void insertionSortArray(int[], int);
+int linearSearchArray(int[], int, int);
+
+int main() {
+	int nums[] = {7, 4, 6, 9, 10, 2, 5, 3, 8};
+	int size = 9;
+
+	printArray(nums, size);
+	insertionSortArray(nums, size);
+	printArray(nums, size);
+	int in = linearSearchArray(nums, size, 9);
+	cout << in << endl;
+
+	return 0;
+}
+
+void insertionSortArray(int arr[], int size) {
+	int i, j, currValue; // other += 3
+	// i = 1 loopControlAssignment++
+	for (i = 1; i < size; i++) {
+		// i < size loopControlComparisons++
+		currValue = arr[i];
+		j = i - 1;
+		while (j >= 0 && currValue < arr[j]) {
+			arr[j + 1] = arr[j];
+			j--;
+		}
+		arr[j + 1] = currValue;
+	}
+	// i < size loopControlComparisons++
+	// for the one time the BC evaluates to false
+}
+
+void printArray(int arr[], int size) {
+	int i;
+	for (i = 0; i < size - 1; i++) {
+		cout << arr[i] << ", "; 
+	}
+	cout << arr[i] << endl;
+	
+}
+
+int linearSearchArray(int arr[], int n, int target)
+{
+	int i;
+	for ( i =0; i<n; i++)
+	{
+		if (arr[i]== target)
+		{
+			return i;
+		}
+	}
+return -1;
+}
